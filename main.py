e795c6d209e5e138ed904117d91bbb8cf3717693
happyinfrance10 <happyinfrance10@users.noreply.github.com>
2018-11-03 14:17:21 -0700

    Skeleton python code for MainPage

--- main.py
+++ main.py
@@ -4,4 +4,18 @@ import os
 import logging
 import time
 
-from google.appengine.ext import 
+from google.appengine.ext import ndb
+
+env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions = ['jinja2.ext.autoescape'],
+    autoescape = True
+)
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        # fetch that question, and return that key and pass as a template variable.
+        template = env.get_template("index.html")
+        templateVars = {
+        }
+        self.response.write(template.render(templateVars))
