0e6ccac5739898c65124eb1eebd99505872b3ff4
Julia Cordero <juliaraquel@airbears2-10-142-120-37.airbears2.1918.berkeley.edu>
2018-11-03 19:25:21 -0700

    more imports

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import json
 #^unsure if those last three imports are needed but the docs said they were
 
 from google.cloud import language
+from google.cloud.language import types
+from google.cloud.language import enums
 from google.appengine.ext import ndb
 
 env = jinja2.Environment(
