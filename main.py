48e09a4c5744173144ed398d167d47049f5088c9
Julia Cordero <juliaraquel@airbears2-10-142-120-37.airbears2.1918.berkeley.edu>
2018-11-03 19:17:55 -0700

    imports for content classification

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import io
 import json
 #^unsure if those last three imports are needed but the docs said they were
 
+from google.cloud import language
 from google.appengine.ext import ndb
 
 env = jinja2.Environment(
